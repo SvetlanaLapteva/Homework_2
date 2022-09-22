52863ee3235cb1c855d740b267f7213daca47f4b
Svetlana Lap <laptevas80@gmail.com>
2022-09-23 00:25:05 +0300

    Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(Math.Abs(((int)a/10)%10));
 
